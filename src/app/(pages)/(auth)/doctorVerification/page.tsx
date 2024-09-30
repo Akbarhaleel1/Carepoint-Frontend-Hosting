ebdd8a0d06d2a40275901166b8b852ae6dd8f120
Akbar Haleel <akbarhaleel508@gmail.com>
2024-09-30 23:02:41 +0530

    test commit5

--- src/app/(pages)/(auth)/doctorVerification/page.tsx
+++ src/app/(pages)/(auth)/doctorVerification/page.tsx
@@ -1,10 +1,13 @@
 
+import { Suspense } from 'react'
 import DoctorVerification from './components/DoctorVerification'
 
 const page = () => {
   return (
     <div>
+        <Suspense fallback={<div>Loading...</div>}>
         <DoctorVerification/>
+        </Suspense>
     </div>
   )
 }
--- src/app/(pages)/user/Appointments/map/page.tsx
+++ src/app/(pages)/user/Appointments/map/page.tsx
@@ -1,11 +1,14 @@
-import MapComponent from "@/app/MapContainer/MapContainers"
+import MapComponent from "@/app/MapContainer/MapContainers";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div>
-      <MapComponent/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MapComponent />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
