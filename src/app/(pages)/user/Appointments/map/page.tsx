79e7fb80734a99be574463e78428f9da64e0efe6
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-01 12:19:53 +0530

    test commit5

--- src/app/(pages)/user/Appointments/map/page.tsx
+++ src/app/(pages)/user/Appointments/map/page.tsx
@@ -1,35 +1,14 @@
-// import MapComponent from "@/app/MapContainer/MapContainers";
-// import { Suspense } from "react";
-
-// const page = () => {
-//   return (
-//     <div>
-//       <Suspense fallback={<div>Loading...</div>}>
-//         <MapComponent />
-//       </Suspense>
-//     </div>
-//   );
-// };
-
-// export default page;
-
-
-// page.tsx
-import dynamic from "next/dynamic";
+import MapComponent from "@/app/MapContainer/MapContainers";
 import { Suspense } from "react";
 
-const MapComponent = dynamic(() => import("@/app/MapContainer/MapContainers"), {
-  ssr: false,
-});
-
-const Page = () => {
+const page = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
-        <MapComponent/>
+        <MapComponent />
       </Suspense>
     </div>
   );
 };
 
-export default Page;
+export default page;
