b00a9a03c17f4a8f147d7e8fd69515bd73050590
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-01 11:59:01 +0530

    try to fix error10

--- src/app/(pages)/user/Appointments/map/page.tsx
+++ src/app/(pages)/user/Appointments/map/page.tsx
@@ -1,36 +1,35 @@
-// // import MapComponent from "@/app/MapContainer/MapContainers";
-// // import { Suspense } from "react";
-
-// // const page = () => {
-// //   return (
-// //     <div>
-// //       <Suspense fallback={<div>Loading...</div>}>
-// //         <MapComponent />
-// //       </Suspense>
-// //     </div>
-// //   );
-// // };
-
-// // export default page;
-
-
-// // page.tsx
-// import dynamic from "next/dynamic";
+// import MapComponent from "@/app/MapContainer/MapContainers";
 // import { Suspense } from "react";
 
-// const MapComponent = dynamic(() => import("@/app/MapContainer/MapContainers"), {
-//   ssr: false,
-// });
-
-// const Page = () => {
+// const page = () => {
 //   return (
 //     <div>
-//       {/* <Suspense fallback={<div>Loading...</div>}>
+//       <Suspense fallback={<div>Loading...</div>}>
 //         <MapComponent />
-//       </Suspense> */}
-//       hello
+//       </Suspense>
 //     </div>
 //   );
 // };
 
-// export default Page;
+// export default page;
+
+
+// page.tsx
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const MapComponent = dynamic(() => import("@/app/MapContainer/MapContainers"), {
+  ssr: false,
+});
+
+const Page = () => {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MapComponent />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Page;
