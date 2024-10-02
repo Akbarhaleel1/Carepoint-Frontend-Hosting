a5ed45acc92858279ac6e3e11216592d5d5c3843
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-02 11:32:55 +0530

    Updated Nginx configuration and related services

--- src/app/hooks/useApi.ts
+++ src/app/hooks/useApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost', 
+  baseURL: 'http://a245a7bb236114c15b4072de9adc4664-1481104342.eu-north-1.elb.amazonaws.com', 
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' } 
 });
