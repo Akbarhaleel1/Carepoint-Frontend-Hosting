77a5c543509d19a1c773dc2b3bfbdaec7d6c3cad
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-03 20:55:47 +0530

    test commit

--- src/app/hooks/useApi.ts
+++ src/app/hooks/useApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://a245a7bb236114c15b4072de9adc4664-1481104342.eu-north-1.elb.amazonaws.com', 
+  baseURL: 'http://localhost', 
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' } 
 });
