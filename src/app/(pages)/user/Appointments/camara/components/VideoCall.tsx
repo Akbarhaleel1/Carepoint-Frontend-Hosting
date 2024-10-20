dc708ed9565454601359622752621904321307df
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-20 19:18:43 +0530

    socket io error fixing4

--- src/app/(pages)/user/Appointments/camara/components/VideoCall.tsx
+++ src/app/(pages)/user/Appointments/camara/components/VideoCall.tsx
@@ -35,16 +35,7 @@ const UserVideoCall = () => {
 
     socketInstance.on('ice-candidate', (data) => {
       console.log('ICE candidate received:', data);
-
-
-      const configuration = {
-        iceServers: [
-            { urls: 'stun:stun.l.google.com:19302' } 
-        ]
-    };
-    const peerConnection = new RTCPeerConnection(configuration);
-
-      // const peerConnection = peerConnectionRef.current;
+      const peerConnection = peerConnectionRef.current;
       if (peerConnection && data.candidate) {
         const candidate = new RTCIceCandidate(data.candidate);
         peerConnection.addIceCandidate(candidate)
@@ -59,14 +50,7 @@ const UserVideoCall = () => {
 
     socketInstance.on('call-created', async (call) => {
       console.log('Call created:', call);
-      // const peerConnection = peerConnectionRef.current;
-      const configuration = {
-        iceServers: [
-            { urls: 'stun:stun.l.google.com:19302' } 
-        ]
-    };
-    const peerConnection = new RTCPeerConnection(configuration);
-
+      const peerConnection = peerConnectionRef.current;
       if (peerConnection) {
         await peerConnection.setRemoteDescription(new RTCSessionDescription(call.offer));
         const answer = await peerConnection.createAnswer();
