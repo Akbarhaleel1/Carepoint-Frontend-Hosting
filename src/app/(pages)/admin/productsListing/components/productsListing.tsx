14c42d1742e0afb1e8e1626b8b6fb05f13ce863d
Akbar Haleel <akbarhaleel508@gmail.com>
2024-10-21 23:55:26 +0530

    delete products

--- src/app/(pages)/admin/productsListing/components/productsListing.tsx
+++ src/app/(pages)/admin/productsListing/components/productsListing.tsx
@@ -34,7 +34,7 @@ const AdminProductListing = () => {
   const handleDelete =async (productId:string)=>{
     console.log('handleDelete',productId)
     const resultForAdminService = await axiosInstance.post('/admin-service/productDelete',{productId});
-    const resultForUserService = await axiosInstance.post('/admin-service/productDelete',{productId});
+    const resultForUserService = await axiosInstance.post('/user-service/productDelete',{productId});
     console.log('resultForAdminService is ', resultForAdminService)
     console.log('resultForUserService is ', resultForAdminService)
   }
